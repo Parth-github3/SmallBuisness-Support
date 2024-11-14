a33e7101dfde350bf3308d0f05914301cf909ee3
Parth Maheshwari <148962439+Parth-github3@users.noreply.github.com>
2024-11-14 22:13:35 +0530

    Update Booking.py

--- Booking.py
+++ Booking.py
@@ -72,9 +72,9 @@ You are a follow-up assistant. Based on the previous interaction: "{previous_int
 
 service = st.text_input("Service Type (e.g., Haircut, Meeting, etc.):")
 date = st.date_input("When to perfom action?", value=None)
-st.write("You have changed to:", d)
+st.write("You have changed to:", date)
 time = st.time_input("Set an alarm for", value=None)
-st.write("Alarm is set for", t)
+st.write("Alarm is set for", time)
 
 
 if service:
