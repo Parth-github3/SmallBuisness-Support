2dec16862055f6ccae56c9e58d72feae27b1ab37
Parth Maheshwari <148962439+Parth-github3@users.noreply.github.com>
2024-11-14 11:41:42 +0530

    Update SB_support.py

--- SB_support.py
+++ SB_support.py
@@ -1,3 +1,14 @@
-import langchain-groq
-import langchain-core
-import streamlit
+#Requirements
+from langchain_groq import ChatGroq
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
+#LLM Model
+llama = ChatGroq(
+    model="LLaMA3-70B-8192",
+    groq_api_key='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',
+    temperature=0.0
+)
+
