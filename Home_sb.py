c45479f128877c016e8c940d7c52d0758c461ef7
Parth Maheshwari <148962439+Parth-github3@users.noreply.github.com>
2024-11-14 21:39:29 +0530

    Update Home_sb.py

--- Home_sb.py
+++ Home_sb.py
@@ -2,28 +2,30 @@ import streamlit as st
 
 # Introduction Text
 st.markdown("""
-### Hello students! Hope you find this app when your exam is near.\n
-This app will make your exam preparations smooth as butter!\n
+### Hello mate!\n
+This app will make your buisness go with a boom\n
         Functionalities; this app provides:\n
-            1. Question
-            2. Answer
-            3. Concept-Learning Plan
+- Multilingual support
+- Booking management
+- Product information retrieval
+- Contextual query handling
+- Customizable responses for businesses
 """)
 
 # How to use section
-st.markdown("**How to use?**")
+# st.markdown("**How to use?**")
 
-with st.expander("Instructions to follow..."):
-    st.markdown("""
-Open the **Question** page -> Upload pyq papers (only in pdf format) -> Download the ***Question_response*** text file.\n 
-You can you this file for both Answer page and Concept-Learning Plan for getting responses.\n
-                """)
+# with st.expander("Instructions to follow..."):
+#     st.markdown("""
+# Open the **Question** page -> Upload pyq papers (only in pdf format) -> Download the ***Question_response*** text file.\n 
+# You can you this file for both Answer page and Concept-Learning Plan for getting responses.\n
+#                 """)
     
 
 # Description
 st.markdown("**Description about app's functions**")
 
-with st.expander("About Question..."):
+with st.expander("About Query..."):
     st.markdown("""
     **Question** is a place where you upload your Past Year Question papers and get your imp questions in seconds.\n
     Input: Question papers(one or more) **Format:** PDF\n
@@ -31,7 +33,7 @@ with st.expander("About Question..."):
     Output: List of imp questions. Also, you can download it as a file.
     """)
 
-with st.expander("About Answer..."):
+with st.expander("About Booking..."):
     st.markdown("""
 In **Answer** page, you will get brief and informative answer for any questions provided. \n
 Input: The ***Question_response*** text file from the **Question** page.\n
@@ -39,7 +41,7 @@ Task: Generation of answers with conceptual understanding.\n
 Output: Answer text file to download.                          
 """)
     
-with st.expander("About Concept-Learning Plan..."):
+with st.expander("About Product Info..."):
     st.markdown("""
 In **Concept-Learning Plan** page, you will get a guided study plan to kickstart your studying.\n
 Input: The ***Question_response*** text file from the **Question** page.\n
